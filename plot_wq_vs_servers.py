5f216a0871623984b1f55cba61b2c88c6844193b
stephenisaac9292 <stephenisaac9292@gmail.com>
2026-02-19 04:38:54 +0100

    graph matplotlib edited

--- plot_wq_vs_servers.py
+++ plot_wq_vs_servers.py
@@ -1,9 +1,9 @@
 import matplotlib.pyplot as plt
 
 # Put the MEAN Wq you got from mm1_sim.py, mm2_sim.py, mm3_sim.py here:
-Wq_mean_c1 = 0.0  # replace
-Wq_mean_c2 = 0.0  # replace
-Wq_mean_c3 = 0.0  # replace
+Wq_mean_c1 = 2262.230043 # replace
+Wq_mean_c2 = 5.032231  # replace
+Wq_mean_c3 = 0.299868  # replace
 
 cs = [1, 2, 3]
 Wqs = [Wq_mean_c1, Wq_mean_c2, Wq_mean_c3]
@@ -14,4 +14,5 @@ plt.xlabel("Number of servers (c)")
 plt.ylabel("Average waiting time in queue (Wq)")
 plt.title("Wq vs Number of Servers")
 plt.grid(True)
-plt.show()
+plt.savefig("wq_vs_servers.png", dpi=150, bbox_inches="tight")
+print("Saved to wq_vs_servers.png")
